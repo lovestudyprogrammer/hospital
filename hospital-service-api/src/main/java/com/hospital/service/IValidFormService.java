77e6d521b23067b5eb88613e6d8e963024d34839
liuyongbin1 <2534849604@qq.com>
2019-02-21 15:33:14 +0800

    刘泳彬-2019/2/21

--- hospital-service-api/src/main/java/com/hospital/service/IValidFormService.java
+++ hospital-service-api/src/main/java/com/hospital/service/IValidFormService.java
@@ -4,4 +4,6 @@ import com.hospital.pojo.User;
 
 public interface IValidFormService {
     User checkExists(String pNumber,String cNumber,String email);
+    int userRegister(User user) throws Exception;
+    User userLogin(User user) throws Exception;
 }
