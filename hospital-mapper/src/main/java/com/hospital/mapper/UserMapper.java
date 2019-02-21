e9993aa3e43841058060ce43d1af5b72c9738241
liuyongbin1 <2534849604@qq.com>
2019-02-21 15:31:09 +0800

    刘泳彬-2019/2/21

--- hospital-mapper/src/main/java/com/hospital/mapper/UserMapper.java
+++ hospital-mapper/src/main/java/com/hospital/mapper/UserMapper.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMapper {
     User checkExists(@Param("pNumber") String pNumber, @Param("cNumber") String cNumber, @Param("email") String email);
+    int insert(@Param("vo") User user);
+    User selectById(String tbId);
+    User selectByCardIdAndPassword(@Param("cardId") String cardId, @Param("password") String password);
 }
