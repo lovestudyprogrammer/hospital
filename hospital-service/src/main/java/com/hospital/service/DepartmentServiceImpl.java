1e70f7eacc6978850a1d610921b9e92881df2571
liuyongbin1 <2534849604@qq.com>
2019-02-26 11:23:23 +0800

    刘泳彬-2019/2/26

--- hospital-service/src/main/java/com/hospital/service/DepartmentServiceImpl.java
+++ hospital-service/src/main/java/com/hospital/service/DepartmentServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -27,32 +26,26 @@ public class DepartmentServiceImpl implements IDepartmentService {
         return list;
     }
 
-    public HashMap<String, List<Doctor>> getDoctors() {
+    public List<DoctorsPOJO> getDoctors() {
         List<Doctor> doctors = doctorMapper.selectAll();
-        DoctorsPOJO doctorsPOJO = new DoctorsPOJO();
-        HashMap<String, List<Doctor>> hashMap = new HashMap<String, List<Doctor>>();
-        Iterator<Doctor> iterator = doctors.iterator();
-        while (iterator.hasNext()){
-            Doctor doctor = iterator.next();
-            //得到名字首字母
-            String firstSpell = PinYinUtil.getFirstSpell(doctor.getDName());
-            //按字典顺序分组
-            for(char a='A'; a<='Z'; a++) {
+        List<DoctorsPOJO> doctorsPOJOArrayList = new ArrayList<DoctorsPOJO>();
+        //按字典顺序分组
+        for(char a='A'; a<='Z'; a++) {
+            List<Doctor> list = new ArrayList<Doctor>();
+            DoctorsPOJO doctorsPOJO = new DoctorsPOJO();
+            doctorsPOJO.setChar(String.valueOf(a));
+            Iterator<Doctor> iterator = doctors.iterator();
+            while (iterator.hasNext()){
+                Doctor doctor = iterator.next();
+                //得到名字首字母
+                String firstSpell = PinYinUtil.getFirstSpell(doctor.getDName());
                 if (String.valueOf(a).equals(firstSpell)) {
-                    if (doctorsPOJO.getChar().equals(firstSpell)) {
-                        List<Doctor> doctorList = doctorsPOJO.getDoctors();
-                        doctorList.add(doctor);
-                        break;
-                    } else {
-                        doctorsPOJO.setChar(firstSpell);
-                        List<Doctor> list = new ArrayList<Doctor>();
-                        list.add(doctor);
-                        hashMap.put(String.valueOf(a), list);
-                        break;
-                    }
+                    list.add(doctor);
                 }
             }
+            doctorsPOJO.setDoctors(list);
+            doctorsPOJOArrayList.add(doctorsPOJO);
         }
-        return hashMap;
+        return doctorsPOJOArrayList;
     }
 }
