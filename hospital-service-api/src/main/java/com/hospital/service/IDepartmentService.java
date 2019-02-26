46a01d4f40d8264247ba333d320bf7f425cc8e59
liuyongbin1 <2534849604@qq.com>
2019-02-26 09:58:42 +0800

    刘泳彬-2019/2/26

--- hospital-service-api/src/main/java/com/hospital/service/IDepartmentService.java
+++ hospital-service-api/src/main/java/com/hospital/service/IDepartmentService.java
@@ -1,9 +1,12 @@
 package com.hospital.service;
 
+import com.hospital.pojo.Doctor;
 import com.hospital.pojo.OfficesPOJO;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface IDepartmentService {
     List<OfficesPOJO> getOffices();
+    HashMap<String, List<Doctor>> getDoctors();
 }
